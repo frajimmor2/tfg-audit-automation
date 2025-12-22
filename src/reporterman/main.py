40288d7ac5468bdc49962304fc3414b3dc1cd3e4
Curro <frajimmor2@alum.us.es>
2025-12-22 13:41:33 +0100

    style: linter refactor

--- src/reporterman/main.py
+++ src/reporterman/main.py
@@ -4,9 +4,11 @@ import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def test(name: str):
     print(f"This is working {name}")
+
 
 @app.command()
 def run(target: str):
@@ -16,6 +18,7 @@ def run(target: str):
 @app.command("setModels")
 def setModels():
     print("Installing all the ollama models")
+
 
 if __name__ == "__main__":
     app()
