67a3d12ce9af0983e2c968a9e0f50cc8f9b3a7c4
Curro <frajimmor2@alum.us.es>
2025-12-24 13:18:06 +0100

    module(reconnaissance): input validations #5

---
+++ src/reporterman/input_validations.py
@@ -0,0 +1,89 @@
+import typer
+
+
+def mode_validation(mode: int) -> int:
+    if not (mode == 0 or mode == 1 or mode == 2):
+        raise typer.BadParameter("Only the modes 0, 1 or 2 are allowed")
+    return mode
+
+
+def valid_IP(target: str) -> None:
+    if target.count(".") != 3:
+        raise typer.BadParameter(
+            f"{target} is not a valid IP. \
+                    Provide the plain IP address, not the CIDR"
+        )
+
+    for num in target.split("."):
+        try:
+            n = int(num)
+        except ValueError:
+            raise typer.BadParameter(
+                f"{target} is not a valid IP. \
+                        Provide the plain IP address, not the CIDR"
+            )
+        if not (0 <= n <= 255):
+            raise typer.BadParameter(
+                f"{target} is not a valid IP. \
+                        Provide the plain IP address, not the CIDR"
+            )
+
+
+def valid_domain(target: str) -> None:
+    if not ("/" in target) or (target.count("/") != 1):
+        raise typer.BadParameter(f"{target} is not a valid IP domain")
+    values = target.split("/")
+    valid_IP(values[0])  # Check well formed IP numeric part
+    try:  # Check the CIDR suffix
+        n = int(values[1])
+    except ValueError:
+        raise typer.BadParameter(f"{values[1]} must be a number")
+    if not (0 <= n <= 32):
+        raise typer.BadParameter("Invalid CIDR suffix")
+
+
+def valid_IP_list(target: str) -> None:
+    if "," not in target:
+        raise typer.BadParameter(
+            "The provided list of IP addresses must \
+                    follow this pattern: IP,IP,IP,IP"
+        )
+    for value in target.split(","):
+        valid_IP(value)
+
+
+def target_validation(target: str, mode: int) -> None:
+    if mode == 0:
+        return valid_IP(target)
+    elif mode == 1:
+        return valid_domain(target)
+    else:
+        return valid_IP_list(target)
+
+
+def check_port(port: str) -> None:
+    if "," not in port:
+        try:
+            n = int(port)
+        except ValueError:
+            raise typer.BadParameter("Port must be an int value")
+
+    if not (0 < n <= 65535):  # Check is a logic port
+        raise typer.BadParameter("Invalid port number")
+
+
+def ports_validation(ports: str) -> None:
+    if ports != "":
+        if "," not in ports:
+            check_port(ports)
+        else:
+            s = set()
+            for port in ports.split(","):
+                check_port(port)
+                # Check they are not duplicated
+                if port not in s:
+                    s.add(port)
+                else:
+                    raise typer.BadParameter(
+                        "Please provide the ports without duplications"
+                    )
--- src/reporterman/main.py
+++ src/reporterman/main.py
@@ -1,18 +1,53 @@
 import typer
-
-# from typing_extensions import Annotated
+from typing_extensions import Annotated
+from reporterman.input_validations import (
+    mode_validation,
+    target_validation,
+    ports_validation,
+)
 
 
 app = typer.Typer()
 
 
 @app.command()
-def test(name: str):
-    print(f"This is working {name}")
-
-
-@app.command()
-def run(target: str):
+def run(
+    target: Annotated[
+        str,
+        typer.Argument(
+            help="The target must be an IP direction, \
+                    IP domain or a list of IPs"
+        ),
+    ],
+    mode: Annotated[
+        int,
+        typer.Option(
+            "-m",
+            "-mode",
+            "--mode",
+            help=(
+                "The mode options specifies which \
+                    target type will be processed.\n"
+                "mode == 0: single IP\n"
+                "mode == 1: IP domain\n"
+                "mode == 2: list of IPs\n"
+            ),
+            callback=mode_validation,
+        ),
+    ] = 0,
+    ports: Annotated[
+        str,
+        typer.Option(
+            "-p",
+            "-ports",
+            "--ports",
+            help="List of the ports that will be audited. \
+                        It applies to each system in the modes 1 and 2",
+        ),
+    ] = "",
+):
+    target_validation(target, mode)
+    ports_validation(ports)
     print("Running")
 
 
